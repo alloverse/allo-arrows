94791cfc34d06fc3c45bc48106d54e3827c02693
Patrik Sjöberg <voxxar@gmail.com>
2021-03-24 15:42:00 +0100

    Bullets per player!

--- lua/main.lua
+++ lua/main.lua
@@ -36,7 +36,6 @@ toViewLocal:invert(toViewLocal)
 
 -- states
 local targets = {}
-local bullets = {}
 local players = {}
 
 local function updateHit(target, hit, bullet)
@@ -69,22 +68,34 @@ for i = 1, 5 do
 end
 
 
-local function addPlayer(id)
-    print("adding player ".. id)
+local function getPlayer(sender_or_id)
+    if type(sender_or_id) == "string" then
+        return players[sender_or_id]
+    end
+    local player = sender_or_id:getParent() or sender_or_id
+    local id = player.id
+    if players[id] then 
+        return players[id]
+    end
+    local name = player.components.visor.display_name or id
+    print("adding player " .. name .. "(" .. id .. ")")
     local crosshairs = ui.Surface(ui.Bounds(0, 0, 0.1,  0.3, 0.3, 0.3))
     crosshairs.color = {1, 0, 0, 1}
-    crosshairs.grabbable = true
     crosshairs.texture = assets.crosshairs
     crosshairs.hasTransparency = true
 
     mainView:addSubview(crosshairs)
     players[id] = {
+        name = name,
         view = crosshairs,
         lastSeen = os.time(),
         aim = {
             from = vec3(),
             to = vec3()
         },
+        bullets = {},
+        score = 0,
+        maxBullets = (name == "Voxar" or name == "Keanu") and 10 or 1,
     }
     return players[id]
 end
@@ -97,19 +108,19 @@ local function shoot(player)
     -- initial velocity
     bullet.v = (player.aim.to - player.aim.from) * 4
     mainView:addSubview(bullet)
-    while #bullets > 2 do
+    local bullets = player.bullets
+    while #bullets > player.maxBullets do
         bullets[1]:removeFromSuperview()
         table.remove(bullets, 1)
     end
     table.insert(bullets, bullet)
 end
 
-
 mainView.onInteraction = function (self, inter, body, sender)
     View.onInteraction(self, inter, body, sender)
     
     if body[1] == "point" then
-        local player = players[sender.id] or addPlayer(sender.id)
+        local player = getPlayer(sender)
         local p = vec3.new(table.unpack(body[3]))
         local mp = toViewLocal * p
         
@@ -125,7 +136,7 @@ mainView.onInteraction = function (self, inter, body, sender)
     end
 
     if body[1] == "poke" and body[2] == true then
-        local player = players[sender.id] or addPlayer(sender.id)
+        local player = getPlayer(sender)
         shoot(player)
         
         player.view:setBounds()
@@ -147,37 +158,38 @@ local gameOver = false
 local dt = 0.03
 app:scheduleAction(dt, true, function()
     if app.connected and animate then
-        -- update the crosshairs
+        -- update player crosshairs and bullets
         for _, player in pairs(players) do
             player.view:setBounds()
-        end
-        for i, bullet in ipairs(bullets) do
-            -- update bullet position
-            bullet.bounds.pose:move(bullet.v.x*dt, bullet.v.y*dt, bullet.v.z*dt)
+            local bullets = player.bullets
+            for i, bullet in ipairs(bullets) do
+                -- update bullet position
+                bullet.bounds.pose:move(bullet.v.x*dt, bullet.v.y*dt, bullet.v.z*dt)
 
-            -- get the bullet position to check against targets
-            local pos = vec3.new(
-                bullet.bounds.pose.transform[13],
-                bullet.bounds.pose.transform[14],
-                bullet.bounds.pose.transform[15]
-            )
+                -- get the bullet position to check against targets
+                local pos = vec3.new(
+                    bullet.bounds.pose.transform[13],
+                    bullet.bounds.pose.transform[14],
+                    bullet.bounds.pose.transform[15]
+                )
 
-            -- Check if it is below the ground, remove it if it is
-            if pos.y < -3 then
-                bullet:removeFromSuperview()
-                table.remove(bullets, i)
-            else
-                -- Otherwise update bullets velocity; add gravity
-                bullet.v.y = bullet.v.y - 9.82 * 0.03
-                bullet:setBounds()
+                -- Check if it is below the ground, remove it if it is
+                if pos.y < -3 then
+                    bullet:removeFromSuperview()
+                    table.remove(bullets, i)
+                else
+                    -- Otherwise update bullets velocity; add gravity
+                    bullet.v.y = bullet.v.y - 9.82 * 0.03
+                    bullet:setBounds()
 
-                -- Did it hit anything?
-                for _, target in ipairs(targets) do
-                    if vec3.dist(target.pos, pos) < 1 then 
-                        updateHit(target, true, bullet)
-                        -- remove bullet if it hit a target
-                        bullet:removeFromSuperview()
-                        table.remove(bullets, i)
+                    -- Did it hit anything?
+                    for _, target in ipairs(targets) do
+                        if vec3.dist(target.pos, pos) < 1 then 
+                            updateHit(target, true, bullet)
+                            -- remove bullet if it hit a target
+                            bullet:removeFromSuperview()
+                            table.remove(bullets, i)
+                        end
                     end
                 end
             end
@@ -210,7 +222,7 @@ app:scheduleAction(2, true, function()
     local time = os.time()
     for id, player in pairs(players) do
         if player.lastSeen + 20 < time then 
-            print("removing player " .. id)
+            print("removing player " .. player.name .. "(" .. id .. ")")
             player.view:removeFromSuperview()
             players[id] = nil
         end
